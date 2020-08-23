cac77dd5d4290a0fa5067c3836d9ab6ebd557555
Bwuckster <bwuck@bwuck.com>
2020-08-22 19:32:38 -0700

    Check whether running tests conditionally works for platforms.

---
+++ tests/stub.c
@@ -0,0 +1,57 @@
+/*
+ * stub.c
+ *
+ *
+ * --------------------------------------------------------------------------
+ *
+ *      Pthreads-win32 - POSIX Threads Library for Win32
+ *      Copyright(C) 1998 John E. Bossom
+ *      Copyright(C) 1999,2005 Pthreads-win32 contributors
+ *
+ *      Contact Email: rpj@callisto.canberra.edu.au
+ *
+ *      The current list of contributors is contained
+ *      in the file CONTRIBUTORS included with the source
+ *      code distribution. The list can also be seen at the
+ *      following World Wide Web location:
+ *      http://sources.redhat.com/pthreads-win32/contributors.html
+ *
+ *      This library is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU Lesser General Public
+ *      License as published by the Free Software Foundation; either
+ *      version 2 of the License, or (at your option) any later version.
+ *
+ *      This library is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *      Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public
+ *      License along with this library in the file COPYING.LIB;
+ *      if not, write to the Free Software Foundation, Inc.,
+ *      59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ *
+ * --------------------------------------------------------------------------
+ *
+ * Stub tests for non-Windows platforms that may attempt to run our tests.
+ * If these tests are run on Windows, you'll see an error.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (void)
+{
+#if defined(_WIN32) || defined(_WIN64)
+  fputs ("error: this is a stub test. are you sure you included the "
+         "correct tests?\n",
+         stderr);
+  return EXIT_FAILURE;
+#else
+  fputs ("note: no tests were run because the platform is not Windows.\n",
+         stderr);
+  return EXIT_SUCCESS;
+#endif
+}
